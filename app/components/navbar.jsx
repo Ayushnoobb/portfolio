6f8ac43d117975d64e2fe4e2aa08cd48b6fae84b
ayush <ayush@parijat.asia>
2023-03-26 16:18:22 +0545

    adj

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -7,7 +7,7 @@ const marker = Permanent_Marker({ subsets: ['latin'] , weight:"400"})
 export default function Navbar() {
   return (
     <nav className="nav py-6 px-8 bg-navy" id="nav">
-      <div className="flex justify-between container my-0 mx-auto text-slate-300">
+      <div className="flex justify-evenly container my-0 mx-auto text-slate-300 items-center">
         <h1 className="text-2xl font-bold">
           <span className={marker.className}>
             Portfolio
@@ -27,7 +27,7 @@ export default function Navbar() {
             <Link href={"/contact"}>Contact</Link>
           </li>
         </ul>
-        <ul className="items-center gap-6 hidden md:flex">
+        <ul className="items-center gap-6 flex justify-center">
           <li className="social-media">
             <Link href={"https://www.facebook.com/profile.php?id=100074470688895"} target={"_blank"}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15  37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84 v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"></path></svg>
